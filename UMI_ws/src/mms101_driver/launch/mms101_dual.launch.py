227c3d5839915a876c61a21fa54c608d855734a4
Hodaka Nakamura <htoytoytok@gmail.com>
2026-08-03 17:09:37 +0900

    add artifacts of the latest version of XGBoost models

--- UMI_ws/src/mms101_driver/launch/mms101_dual.launch.py
+++ UMI_ws/src/mms101_driver/launch/mms101_dual.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
         'port1', default_value='/dev/serial/by-id/usb-FTDI_FT230X_Basic_UART_DK0DNDXM-if00-port0', description='Serial port for MMS101 #1'
     )
     port2_arg = DeclareLaunchArgument(
-        'port2', default_value='/dev/serial/by-id/usb-FTDI_FT230X_Basic_UART_D2019A8H-if00-port0', description='Serial port for MMS101 #2'
+        'port2', default_value='/dev/serial/by-id/usb-FTDI_FT230X_Basic_UART_D20KA4AS-if00-port0', description='Serial port for MMS101 #2'
     )
     frame1_arg = DeclareLaunchArgument(
         'frame1', default_value='ft_sensor_left', description='frame_id for MMS101 #1'
